1f472ab7edeb645225d15af3c47fc21fc2e75753
abiam <abiamrg.14@gmail.com>
2022-01-21 09:56:27 +0000

    Preparamos una nueva prueba

--- src/test/java/dis/ulpgc/es/ProductFibCalculatorTest.java
+++ src/test/java/dis/ulpgc/es/ProductFibCalculatorTest.java
@@ -12,6 +12,7 @@ public class ProductFibCalculatorTest {
     Integer[] PF_714;
     Integer[] PF_1870;
     Integer[] PF_1;
+    Integer[] PF_800;
     @Before
     public void setup(){
         productFibCalculator = new ProductFibCalculator();
@@ -20,13 +21,15 @@ public class ProductFibCalculatorTest {
         PF_714 = new Integer[]{21,34,1};
         PF_714 = new Integer[]{21,34,1};
         PF_1870 = new Integer[]{34, 55, 1};
-
+        PF_800 = new Integer[]{34, 55, 0};
     }
+
     @Test
-    public void return_fibonacci_when_product_is_cero(){
+    public void return_fibonacci_sequence_of_a_product(){
         assertArrayEquals(PF_0,productFibCalculator.productFib(0));
         assertArrayEquals(PF_1,productFibCalculator.productFib(1));
         assertArrayEquals(PF_714,productFibCalculator.productFib(714));
         assertArrayEquals(PF_1870,productFibCalculator.productFib(1870));
+        assertArrayEquals(PF_800,productFibCalculator.productFib(800));
     }
 }
