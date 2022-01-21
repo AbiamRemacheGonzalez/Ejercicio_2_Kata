5da73e8ece4ec8c25b79ec69ee5b282fd6ed54f1
abiam <abiamrg.14@gmail.com>
2022-01-21 10:01:53 +0000

    Refactorizamos el código para que pase la prueba con producto 800

--- src/main/java/dis/ulpgc/es/ProductFibCalculator.java
+++ src/main/java/dis/ulpgc/es/ProductFibCalculator.java
@@ -5,12 +5,9 @@ public class ProductFibCalculator {
     public Integer[] productFib(Integer product){
         Integer[] productFib  = new Integer[]{0,1,0};
         for (int i = 0; i <= product; i++) {
-            if(fibonacciCalculator.getFibonacci(i) * fibonacciCalculator.getFibonacci(i+1)>product){
-                productFib[2]=0;
-                break;
-            }
             productFib[0]=fibonacciCalculator.getFibonacci(i);
             productFib[1]=fibonacciCalculator.getFibonacci(i+1);
+            if(fibonacciCalculator.getFibonacci(i) * fibonacciCalculator.getFibonacci(i+1)>product) break;
             if(fibonacciCalculator.getFibonacci(i) * fibonacciCalculator.getFibonacci(i+1)==product){
                 productFib[2]=1;
                 break;
